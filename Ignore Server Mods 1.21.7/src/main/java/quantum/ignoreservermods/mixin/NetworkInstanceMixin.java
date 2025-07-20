354ea1e8e36d647ca57071f8994b3af2a65db243
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2025-07-20 17:41:22 +0200

    added support for 1.21.7 & 1.21.8

---
+++ Ignore Server Mods 1.21.7/src/main/java/quantum/ignoreservermods/mixin/NetworkInstanceMixin.java
@@ -0,0 +1,17 @@
+package quantum.ignoreservermods.mixin;
+
+import net.neoforged.neoforge.network.negotiation.NegotiationResult;
+import net.neoforged.neoforge.client.network.registration.ClientNetworkRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import quantum.ignoreservermods.Config;
+
+@Mixin(ClientNetworkRegistry.class)
+public class NetworkInstanceMixin {
+
+    @Redirect(method = "initializeOtherConnection(Lnet/minecraft/network/protocol/configuration/ClientConfigurationPacketListener;)V",at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/network/negotiation/NegotiationResult;success()Z"))
+    private static boolean fix(NegotiationResult instance){
+        return Config.IgnoreVanillaServerValue || instance.success();
+    }
+}
